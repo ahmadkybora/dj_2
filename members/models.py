73be121a31da041bf29f3b59f2cc7888791aa77c
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-09-06 09:31:23 +0430

    ...

--- members/models.py
+++ members/models.py
@@ -1,6 +1,24 @@
-from pyexpat import model
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
+class Member(models.Model):
+    first_name = models.CharField(max_length=200, verbose_name="نام")
+    last_name = models.CharField(max_length=200, verbose_name="نام خانوادگی")
     username = models.CharField(max_length=200, verbose_name="نام کاربری")
+    mobile = models.CharField(max_length=200, verbose_name="موبایل")
+
+    def __str__(self) -> str:
+        return "%s %s" % (self.first_name, self.last_name)
+
+    def getAll(self):
+        return self.objects.all()
+
+    def create(self, data=[]):
+        self.objects.create(data)
+        return self.save()
+
+    def getById(self, data):
+        return self.objects.get(data=data)
+
+    def ascOrdesc(self, data):
+        return self.objects.all().orderby(data)
